a24e3b0355bcb26a14f9d2bf4a001bcff8d1e1c6
jung <mira20932@gmail.com>
2022-11-12 19:22:37 +0900

    feat: add AdminEnroll

---
+++ src/components/UrlTextField.jsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import FormControl, { useFormControl } from '@mui/material/FormControl';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Box from '@mui/material/Box';
+import FormHelperText from '@mui/material/FormHelperText';
+
+function MyFormHelperText() {
+  const { focused } = useFormControl() || {};
+
+  const helperText = React.useMemo(() => {
+    if (focused) {
+      return '';
+    }
+
+    return '';
+  }, [focused]);
+
+  return <FormHelperText>{helperText}</FormHelperText>;
+}
+
+export default function UseFormControl() {
+  return (
+    <Box component="form" noValidate autoComplete="off" sx={{ width: '964px', height: '85px'}}>
+      <FormControl fullWidth sx={{ width: '964px', height: '85px' }} style={{ backgroundColor: "#FFFFFF" , borderColor: "#FFFFFF"}}>
+        <OutlinedInput placeholder="URL을 입력해주세요" />
+        <MyFormHelperText />
+      </FormControl>
+    </Box>
+  );
+}
--- src/pages/AdminEnroll.jsx
+++ src/pages/AdminEnroll.jsx
@@ -1,10 +1,69 @@
 import React from "react";
 import Drawer from "../components/Drawer";
-import { Box, Typography } from '@mui/material';
+import UrlTextField from "../components/UrlTextField";
+import HTMLTextField from "../components/HtmlTextField";
+import Popup from "../components/Popup";
+import { Box, Button, Typography } from '@mui/material';
 import { borderRadius } from "@mui/system";
 
-export default function AdminEnroll() {
+function AdminEnroll() {
     return(
-        <Drawer></Drawer>
+        <Box sx={{ overflow: 'outo',
+            flexDirection: 'column'
+        }}>
+            <Drawer />
+            <div style={{ display: 'flex', 
+                              flexDirection: 'row',
+                              marginInlineStart:'118px'}}>
+                    <Typography 
+                        sx={{
+                            width: '235px',
+                            height: '73px',
+                            fontFamily: 'Inter',
+                            fontStyle: 'normal',
+                            fontWeight: '700',
+                            fontSize: '60px',
+                            lineHeight: '73px',
+                            textAlign: 'center',
+                            marginTop:'19px'
+                        }}>
+                        강좌 등록
+                    </Typography>
+                    <Typography sx={{
+                        width: '359px',
+                        height: '36px',
+                        fontFamily: 'Inter',
+                        fontStyle: 'normal',
+                        fontWeight: '400',
+                        fontSize: '30px',
+                        lineHeight: '36px',
+                        marginTop:'45px'
+                    }}>
+                        강좌를 등록하세요
+                     </Typography>
+               </div>
+               <><div style={{  display: 'flex',
+                                flexDirection: 'column', 
+                                rowGap: '100px', 
+                                marginTop: '100px',
+                                marginInlineStart:'167px', 
+                                marginInlineEnd: '118px', 
+                                justifycontent: 'space-around'}}>
+
+                    <UrlTextField/>
+                    <HTMLTextField/>
+                   
+                </div></>
+                <><div style={{  display: 'flex',
+                                flexDirection: 'column', 
+                                rowGap: '100px', 
+                                marginTop: '100px',
+                                marginInlineStart:'600px', 
+                                marginInlineEnd: '118px', 
+                                justifycontent: 'center'}}>   
+                <Popup/>
+                </div></>
+        </Box>
     );
 }
+export default AdminEnroll;
