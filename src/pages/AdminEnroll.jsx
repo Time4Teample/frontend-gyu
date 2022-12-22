eea565520f6f7fc57b9bfa20472f3fe3f6a86ba3
Joohee.K <wnglgl22@tukorea.ac.kr>
2022-12-22 21:03:29 +0900

    feat: route&add page

--- src/pages/AdminEnroll.jsx
+++ src/pages/AdminEnroll.jsx
@@ -1,14 +1,15 @@
-import { Box, width } from "@mui/system";
+import { Box } from "@mui/system";
 import React from "react";
 import Drawer from "../components/Drawer";
 import TextField from "../components/TextField";
 
-export default function AdminMain() {
+export default function AdminEnroll() {
     return (
         <><div>
             <Drawer />
-            <TextField />
-            <TextField />
+            <Box>
+                
+            </Box>
         </div></>
     );
 }
