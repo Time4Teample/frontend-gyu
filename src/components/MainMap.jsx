b6096eec37e6eca9e05c1c615702573b684ad077
홍성민 <seanhong2000@gmail.com>
2022-12-28 01:37:40 +0900

    [feat] add map overlay

--- src/components/MainMap.jsx
+++ src/components/MainMap.jsx
@@ -1,16 +1,45 @@
-import { Map as Map_, MapMarker, CustomOverlayMap } from 'react-kakao-maps-sdk';
+import { Map, CustomOverlayMap } from 'react-kakao-maps-sdk';
 import * as React from 'react';
-
+import LectureInfo from './LectureInfo';
 
 export default function MainMap() {
     return (
-        <Map_
-            center={{ lat: 37.4029976, lng: 127.1314306 }}
-            style={{ width: "100%", height: "360px" }}
+        <>
+
+        <Map // 지도를 표시할 Container
+        center={{ lat: 37.4029976, lng: 127.1314306 }}
+        style={{
+          // 지도의 크기
+          width: "100%",
+          height: "100%",
+        }}
+        level={4} // 지도의 확대 레벨
+      >
+        <CustomOverlayMap // 커스텀 오버레이를 표시할 Container
+          // 커스텀 오버레이가 표시될 위치입니다
+          position={{
+            lat: 37.4029976,
+            lng: 127.1314306,
+          }}
+          // 커스텀 오버레이가에 대한 확장 옵션
+          xAnchor={0.3}
+          yAnchor={0.91}
         >
-            <MapMarker position={{ lat: 37.4029976, lng: 127.1314306 }}>
-                <div style={{color:"#000"}}>Hello World!</div>
-            </MapMarker>
-        </Map_>
+          <LectureInfo />
+        </CustomOverlayMap>
+        <CustomOverlayMap // 커스텀 오버레이를 표시할 Container
+          // 커스텀 오버레이가 표시될 위치입니다
+          position={{
+            lat: 37.4029976,
+            lng: 127.1314306,
+          }}
+          // 커스텀 오버레이가에 대한 확장 옵션
+          xAnchor={0.3}
+          yAnchor={0.91}
+        >
+          <LectureInfo />
+        </CustomOverlayMap>
+      </Map>
+      </>
     );
 }
